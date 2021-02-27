462c908e5ee6fb770b7d144b0c651a725f7516c0
slimshady143 <lawhicham@gmail.com>
2021-02-27 16:22:18 +0100

    initial commit

---
+++ apache/source/api/config/database.php
@@ -0,0 +1,26 @@
+<?php
+class Database{
+ 
+    // specify your own database credentials
+    private $host = "172.17.0.2";
+    private $db_name = "hospital_db";
+    private $username = "root";
+    private $password = "password";
+    public $conn;
+ 
+    // get the database connection
+    public function getConnection(){
+ 
+        $this->conn = null;
+ 
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+ 
+        return $this->conn;
+    }
+}
+?>
